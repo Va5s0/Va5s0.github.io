2ebf5e3043fc48d56f40a41141b81bbb6ae9955e
Va5s0 <vassog@gmail.com>
2016-10-15 20:10:52 +0300

    tidy up code + disable animation from IE11,10

--- js/aboutSection_animate.js
+++ js/aboutSection_animate.js
@@ -1,7 +1,7 @@
 /* adds animation @ about section using viewportChecker.js script */
 
 $(document).ready(function($){
-  if(! /IEMobile/i.test(navigator.userAgent) ) {
+  if(! /IEMobile|MSIE 10.0|rv:11.0/i.test(navigator.userAgent) ) {
     $('.animate_left').addClass("hidden_animate").viewportChecker({
       classToAdd: 'visible_animate animated fadeInLeft', // Class to add to the elements when they are visible
       offset: 100
